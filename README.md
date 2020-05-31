2772f67b4ec3749e168a339a1ec31ed3097e65d9
derek <frankderekdick@gmail.com>
2020-05-31 15:46:50 +0800

    Adds the initial grids.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # reinforce-py
 
 [reference](https://codeburst.io/full-stack-single-page-application-with-vue-js-and-flask-b1e036315532)
+
+TODO:
+- [ ] Grids are not perfectly square.
--- frontend/src/components/GridWorld.vue
+++ frontend/src/components/GridWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="grid-world">
-    <div class="grid-container"></div>
+    <div class="grid-container">
+      <div class="grid-row" v-for="i in 10">
+        <div class="grid" v-for="i in 10">{{ i }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,9 +19,33 @@ export default {
 <style scoped>
 .grid-world {
   font-family: "roboto";
+  box-sizing: border-box;
+  width: 40%;
+  margin: 0 auto;
 }
 
 .grid-container {
-  width: 60%;
+  width: 40vw;
+  height: 40vw;
+  background-color: #2980b9;
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+}
+
+.grid-row {
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  width: 100%;
+  height: 10%;
+
+  background-color: #f62459;
+}
+
+.grid {
+  align-self: stretch;
+  flex-grow: 1;
 }
 </style>
