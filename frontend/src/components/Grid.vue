90908d773b75488bd7aa9898db9448be35c16f9f
derek <frankderekdick@gmail.com>
2020-05-31 16:20:13 +0800

    Adds the Grid component.

---
+++ frontend/src/components/Grid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="grid">
+    {{gridData.gridIndex}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Grid',
+  model: {
+    prop: 'gridData',
+    event: 'onGridClicked'
+  },
+  props: {
+    gridData: {
+      type: Object, // gridIndex: 99, actionArray: [ 0, 1, 2, 3 ]
+      required: true
+    }
+  },
+  data: function () {
+    return {}
+  },
+  methods: {
+    onGridClicked: function (index) {
+      this.$emit('onGridClicked', index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.grid {
+  align-self: stretch;
+  flex-grow: 1;
+}
+</style>
--- frontend/src/components/GridWorld.vue
+++ frontend/src/components/GridWorld.vue
@@ -2,17 +2,37 @@
   <div class="grid-world">
     <div class="grid-container">
       <div class="grid-row" v-for="i in 10">
-        <div class="grid" v-for="i in 10">{{ i }}</div>
+        <grid v-for="j in 10" v-model="gridDataArray[(i - 1) * 10 + j - 1]" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Grid from './Grid';
+
 export default {
   name: 'GridWorld',
   props: {
   },
+  components: { Grid },
+  data: function () {
+    return {
+      gridDataArray: [],
+    }
+  },
+  created () {
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        this.gridDataArray.push(
+          {
+            gridIndex: i * 10 + j,
+            actionArray: [ 0, 1, 2, 3 ]
+          }
+        );
+      }
+    }
+  }
 };
 </script>
 
