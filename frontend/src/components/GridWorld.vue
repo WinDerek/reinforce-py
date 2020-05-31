933a2ab7b1cdd58904e3a2880e4a9a5ce7cba483
derek <frankderekdick@gmail.com>
2020-05-31 10:00:32 +0800

    Initial commit.

---
+++ frontend/src/components/GridWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid-world">
+    <h1>Welcome to Grid World</h1>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridWorld',
+  props: {
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <GridWorld />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GridWorld from '@/components/GridWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    GridWorld,
+  },
+};
+</script>
